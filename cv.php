bbd3bbe9d840f741978a16cd10cba0487cf474c1
SemiZhuri <semizhuri36@gmail.com>
2024-01-29 23:52:35 +0100

    error fixes

--- cv.php
+++ cv.php
@@ -1,11 +1,7 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 session_start();
 include 'classes/dbh.classes.php';
-include 'includes/displayImage.inc.php';
 require_once 'classes/cv.classes.php';
 if(isset($_SESSION["userid"])){
     
